3e162fb4877ffb78bef772ee4ca525b9204be929
cmsanju <lict.sanju@gmail.com>
2022-04-14 20:17:08 +0530

    change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { StudentlistComponent } from './studentlist/studentlist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home-page', component: HomeComponent},
+  {path: 'student-list', component:StudentlistComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
